6ecabecc528372984c4b68f0c7fc93e76e4d79a5
sandeep3135 <sandeep7808822926@gmail.com>
2024-08-02 11:19:45 +0530

    dummy push

--- server/database/db.js
+++ server/database/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
     const URL =`mongodb://${USERNAME}:${PASSWORD}@ac-6rzvsjf-shard-00-00.9kzbkwf.mongodb.net:27017,ac-6rzvsjf-shard-00-01.9kzbkwf.mongodb.net:27017,ac-6rzvsjf-shard-00-02.9kzbkwf.mongodb.net:27017/?ssl=true&replicaSet=atlas-payyus-shard-0&authSource=admin&retryWrites=true&w=majority&appName=blog-web`;
     try{
         await mongoose.connect(URL);
-        console.log("Database connected successfully"); //if database connected
+        console.log("Database connected successfully!"); //if database connected
     }catch (error){
        console.log("Error while connecting with database",error);//if not then
     }
